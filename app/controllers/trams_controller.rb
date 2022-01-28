048179b8ba12d5c754b2d6ad1b2a3937233c427b
Mia Jindrichova <lucie.jindrichova@realpadsoftware.com>
2022-01-28 15:04:27 +0100

    Select route number

--- app/controllers/trams_controller.rb
+++ app/controllers/trams_controller.rb
@@ -1,4 +1,14 @@
 class TramsController < ApplicationController
   def index
+    @trams = Route.all
+  end
+
+  def tram_select
+    redirect_to action: :show_order, id: params[:id]
+  end
+
+  def show_order
+    @trams = Route.all
+    @tram = Route.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root "trams#index"
 
   get "/trams", to: "trams#index"
+  post "/tram", to: "trams#tram_select"
+  get "/tram/:id", to: "trams#show_order"
 
 end
