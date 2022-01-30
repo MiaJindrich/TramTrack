bfe45c5d7e58633283c5639c5e421245f375747f
Mia Jindrichova <lucie.jindrichova@realpadsoftware.com>
2022-01-30 16:15:56 +0100

    Select order number

--- app/controllers/trams_controller.rb
+++ app/controllers/trams_controller.rb
@@ -4,11 +4,20 @@ class TramsController < ApplicationController
   end
 
   def tram_select
-    redirect_to action: :show_order, id: params[:id]
+    redirect_to action: :show_order, route_id: params[:route_id]
   end
 
   def show_order
     @trams = Route.all
-    @tram = Route.find(params[:id])
+    @tram = Route.find(params[:route_id])
+    @trips = Trip.where("route_id = ?", params[:route_id])
+  end
+
+  def order_select
+    redirect_to action: :location, route_id: params[:route_id], trip_id: params[:trip_id]
+  end
+
+  def location
+
   end
 end
